bedd8a8736707ded37bfd3bfdd9883e7da9c8420
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-06-04 21:26:19 +0000

    Icon margin

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -6,11 +6,15 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Column(
-        children: [
-          Icon(Icons.list),
-          Text('Todoey'),
-        ],
+      body: Container(
+        padding:
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+        child: Column(
+          children: [
+            Icon(Icons.list),
+            Text('Todoey'),
+          ],
+        ),
       ),
     );
   }
