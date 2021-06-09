2c741990afdb400a849b4298778b2a0b772bc662
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-06-09 21:38:12 +0000

    Increments

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,7 +5,7 @@ class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      backgroundColor: Colors.lightBlueAccent, //colour of the background and app bar
       body: Container(
         padding:
             EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0), // the edges
