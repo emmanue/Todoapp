88aac01b03c3b68ac442b5a725c4f01dd4c366fb
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-06-14 20:03:50 +0000

    Designing List card

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // declarative user interface
@@ -6,19 +7,63 @@ class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          Colors.lightBlueAccent, //colour of the background and app bar
-      body: Container(
-        // container widget
-        padding: EdgeInsets.only(
-            top: 60.0, left: 30.0, right: 30.0, bottom: 30.0), // the edges
-        child: Column(
-          children: [
-            // widget
-            CircleAvatar(child: Icon(Icons.list)), // Icon on the app bar
-            Text('Todoey'), // Appear on the app bar
-          ],
-        ),
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ), //colour of the background and app bar
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            // container widget
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0), // the edges
+            child: Column(
+              children: [
+                // widget
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ), // Icon on the app bar
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
