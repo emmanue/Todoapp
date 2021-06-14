c78c5289ea2ed873427f322e2781da366933e1fa
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-06-14 19:24:44 +0000

    Circle Avatar in app bar

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 // declarative user interface
-class TasksScreen extends StatelessWidget { // stateless
+class TasksScreen extends StatelessWidget {
+  // stateless
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent, //colour of the background and app bar
-      body: Container( // container widget
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0), // the edges
+      backgroundColor:
+          Colors.lightBlueAccent, //colour of the background and app bar
+      body: Container(
+        // container widget
+        padding: EdgeInsets.only(
+            top: 60.0, left: 30.0, right: 30.0, bottom: 30.0), // the edges
         child: Column(
-          children: [ // widget
-            Icon(Icons.list),// Icon on the app bar
+          children: [
+            // widget
+            CircleAvatar(child: Icon(Icons.list)), // Icon on the app bar
             Text('Todoey'), // Appear on the app bar
           ],
         ),
