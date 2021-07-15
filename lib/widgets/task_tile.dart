86fdf2fba02e1b5fa8fd0c349cde5c7f24066d90
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-07-15 21:40:05 +0000

    Increment

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -32,7 +32,7 @@ class _TaskTileState extends State<TaskTile> {
 
 // Passed the value to final through a constructor callback
 // Constructor is a valid way to proceed
-// well structured
+// well structured and developed
 class TaskCheckbox extends StatelessWidget {
   final bool checkboxState;
   final Function toggleCheckboxState;
