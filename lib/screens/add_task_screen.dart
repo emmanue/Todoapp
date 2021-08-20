a4b0d01823d77dc14dc9d62ca85a84fcd1f0236e
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-08-20 21:04:37 +0000

    Increment

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// Making our code readability improved
 // Following good design patterns
 // Well documented and organized with imports from cupertino and material design
 class AddTaskScreen extends StatelessWidget {
