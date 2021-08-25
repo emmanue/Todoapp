2ac851a7d2f18d06a4ab18cea87288c823f064a3
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-08-25 21:31:16 +0000

    Increment

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:todoey_flutter/screens/tasks_screen.dart';
 void main() {
   runApp(MyApp());
 }
+
+// Good architecture of the app
 // Well structure good following good design
 // Separation of concern well done
 // Readability following best practices
